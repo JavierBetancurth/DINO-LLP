7dffa3eb73d4e681c25a45a00bbad0332e299438
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2023-08-25 03:23:47 -0500

    Update run_with_TPU.py

--- main_dino_TPU/run_with_TPU.py
+++ main_dino_TPU/run_with_TPU.py
@@ -3,17 +3,15 @@ import os
 import uuid
 from pathlib import Path
 
-import main_dino_TPU
-import submitit
+from main_dino_TPU import main_dino_TPU
 import torch_xla.distributed.xla_multiprocessing as xmp
 
 def parse_args():
-    parser = argparse.ArgumentParser("Submitit for DINO", parents=[main_dino_TPU.get_args_parser()])
+    parser = argparse.ArgumentParser("DINO Training with TPU", parents=[main_dino_TPU.get_args_parser()])
     parser.add_argument("--tpu_num_cores", default=8, type=int, help="Number of TPU cores to use")
 
     parser.add_argument("--use_volta32", action='store_true', help="Big models? Use this")
-    parser.add_argument('--comment', default="", type=str,
-                        help='Comment to pass to scheduler, e.g. priority message')
+    parser.add_argument('--comment', default="", type=str, help='Comment to pass to scheduler, e.g. priority message')
     return parser.parse_args()
 
 class Trainer(object):
@@ -22,7 +20,7 @@ class Trainer(object):
 
     def __call__(self, index):
         self.args.gpu = index
-        main_dino.train_dino(self.args)
+        main_dino_TPU.train_dino(self.args)
 
 def main(index):
     args = parse_args()
@@ -30,7 +28,7 @@ def main(index):
         args.output_dir = f"/content/experiments/{index}"  # Change this to your desired directory
     Path(args.output_dir).mkdir(parents=True, exist_ok=True)
 
-    args.dist_url = f"tcp://localhost:{12345}"  # Change the port as needed
+    args.dist_url = f"env://"
 
     xmp.spawn(Trainer(args), args=(), nprocs=1, start_method='fork')
 
