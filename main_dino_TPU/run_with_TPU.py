e5b11397a8987eac29d5e50fd0c64fcb18b06993
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2023-08-25 03:07:54 -0500

    Update run_with_TPU.py

--- main_dino_TPU/run_with_TPU.py
+++ main_dino_TPU/run_with_TPU.py
@@ -3,7 +3,7 @@ import os
 import uuid
 from pathlib import Path
 
-import main_dino
+from .main_dino_TPU import main_dino_TPU
 import submitit
 import torch_xla.distributed.xla_multiprocessing as xmp
 
