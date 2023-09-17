e57b2a048d9902b4ddb79c4c18f03f0be4c3d3d9
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2023-09-17 17:53:08 -0500

    Update and rename kl_loss to kl_loss.py

---
+++ loss/kl_loss.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+# Label proportions-based loss with symmetric cross entropy
+def compute_kl_loss_on_bagbatch(estimated_proportions, real_proportions, device, epsilon=1e-8):
+    # Move tensors to the configured device
+    estimated_proportions = estimated_proportions.to(device)
+    real_proportions = real_proportions.to(device)
+    # Forward pass
+    batch_size, real_proportions
+    outputs = model(images)
+    prob = nn.functional.softmax(outputs, dim=-1).reshape((batch_size, bag_size, -1))
+    avg_prob = torch.mean(prob, dim=1)
+    avg_prob = torch.clamp(avg_prob, epsilon, 1 - epsilon)
+    loss = torch.sum(-props * torch.log(avg_prob), dim=-1).mean()
+    return loss
+
+
+
+                ce_loss = torch.sum(-real_proportions * F.log_softmax(estimated_proportions), dim=-1)
+                rce_loss = torch.sum(-estimated_proportions, dim=-1) * torch.log(real_proportions)
+                loss2 = ce_loss + beta * rce_loss
