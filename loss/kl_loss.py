a4ce78f54baddf99a6ee2f624d33600b047be576
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2023-09-20 03:12:54 -0500

    Update kl_loss.py

--- loss/kl_loss.py
+++ loss/kl_loss.py
@@ -3,13 +3,16 @@ import torch.nn as nn
 
 # Label proportions-based loss with asymmetric cross entropy
 def compute_kl_loss_on_bagbatch(estimated_proportions, class_proportions_list, epsilon=1e-8):
-    estimated_proportions = estimated_proportions.cuda().requires_grad_()
+    estimated_proportions.clone().detach().requires_grad_(True) 
+    # estimated_proportions = estimated_proportions.cuda().requires_grad_()
     # estimated_proportions = torch.tensor(estimated_proportions, dtype=torch.float32)
-    
+
+    print(estimated_proportions)
     for i in range(len(class_proportions_list)):
         real_proportions = class_proportions_list[i]  # Proporciones reales del lote actual
-        
-        real_proportions = torch.tensor(real_proportions, dtype=torch.float32).cuda()
+
+        real_proportions.clone().detach().requires_grad_(True)
+        # real_proportions = torch.tensor(real_proportions, dtype=torch.float32).cuda()
         
         # Calcular las probabilidades y la pérdida KL
         probabilities = nn.functional.softmax(estimated_proportions, dim=-1)
