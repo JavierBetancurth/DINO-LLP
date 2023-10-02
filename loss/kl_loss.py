3a8c10891f07eeb47fdf1887c0d871c94c76852b
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2023-10-02 01:09:19 -0500

    Update kl_loss.py

--- loss/kl_loss.py
+++ loss/kl_loss.py
@@ -24,6 +24,30 @@ def compute_kl_loss_on_bagbatch(estimated_proportions, class_proportions_list, e
     return loss
 
 
+# Label proportions-based loss with symmetric cross entropy
+def compute_kl_loss_on_bagbatch2(estimated_proportions, class_proportions_list, epsilon=1e-8, beta=1):
+    estimated_proportions.clone().detach().requires_grad_(True) 
+    # estimated_proportions = estimated_proportions.cuda().requires_grad_()
+    # estimated_proportions = torch.tensor(estimated_proportions, dtype=torch.float32)
+
+    for i in range(len(class_proportions_list)):
+        real_proportions = class_proportions_list[i]  # Proporciones reales del lote actual
+
+        #real_proportions.clone().detach().requires_grad_(True)
+        real_proportions = torch.tensor(real_proportions, dtype=torch.float32).cuda()
+        
+        # Calcular las probabilidades y la pérdida KL
+        probabilities = nn.functional.softmax(estimated_proportions, dim=-1)
+        avg_prob = torch.mean(probabilities, dim=0)
+        avg_prob = torch.clamp(avg_prob, epsilon, 1 - epsilon)
+
+        # Calcular la pérdida KL utilizando las proporciones del lote
+        ce_loss = torch.sum(-real_proportions * F.log_softmax(avg_prob), dim=-1).mean
+        rce_loss = torch.sum(-avg_prob, dim=-1) * torch.log(real_proportions).mean
+        loss = ce_loss + beta * rce_loss
+        
+    return loss
+        
 
 # ce_loss = torch.sum(-real_proportions * F.log_softmax(estimated_proportions), dim=-1)
 # rce_loss = torch.sum(-estimated_proportions, dim=-1) * torch.log(real_proportions)
