f39c44a9aee972d07f4163e37679e7bc09caebeb
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2023-10-15 23:40:27 -0500

    Update llp_loss.py

--- loss/llp_loss.py
+++ loss/llp_loss.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions.constraints import simplex
 
-from llp_vat.lib.networks import GaussianNoise
+# from llp_vat.lib.networks import GaussianNoise
 
 
 def compute_soft_kl(inputs, targets):
