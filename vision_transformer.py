544055c127746a24042af802cddccd856bde4347
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2024-09-16 00:45:28 -0500

    Update vision_transformer.py

--- vision_transformer.py
+++ vision_transformer.py
@@ -327,9 +327,9 @@ class DINOHead(nn.Module):
             self.mlp = nn.Sequential(*layers)
         self.apply(self._init_weights)
         self.last_layer = nn.utils.parametrizations.weight_norm(nn.Linear(bottleneck_dim, out_dim, bias=False))
-        self.last_layer.weight_g.data.fill_(1)
+        self.last_layer.parametrizations.weight.original0.data.fill_(1)
         if norm_last_layer:
-            self.last_layer.weight_g.requires_grad = False
+            self.last_layer.parametrizations.weight.original0.requires_grad = False
 
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
@@ -374,12 +374,12 @@ class CrOCHead(nn.Module):
             self.mlp = nn.Sequential(*layers)
         self.apply(self._init_weights)
         self.last_layer = nn.utils.parametrizations.weight_norm(nn.Linear(bottleneck_dim, out_dim, bias=False))
-        self.last_layer.weight_g.data.fill_(1)
+        self.last_layer.parametrizations.weight.original0.data.fill_(1)
         self.last_layer_c = nn.utils.parametrizations.weight_norm(nn.Linear(bottleneck_dim, out_dim_c, bias=False))
-        self.last_layer_c.weight_g.data.fill_(1)
+        self.last_layer_c.parametrizations.weight.original0.data.fill_(1)
         if norm_last_layer:
-            self.last_layer.weight_g.requires_grad = False
-            self.last_layer_c.weight_g.requires_grad = False
+            self.last_layer.parametrizations.weight.original0.requires_grad = False
+            self.last_layer_c.parametrizations.weight.original0.requires_grad = False
 
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
