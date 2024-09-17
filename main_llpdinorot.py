c27ea4adf7a21d98476b50837dfd945a133d9841
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2024-09-17 11:55:57 -0500

    Update main_llpdinorot.py

--- main_llpdinorot.py
+++ main_llpdinorot.py
@@ -330,7 +330,6 @@ def calculate_class_proportions_in_batch(labels, dataset):
     class_proportions = class_counts / len(labels)
     return class_proportions
 
-'''
 def compute_kl_loss_on_bagbatch(estimated_proportions, class_proportions, epsilon=1e-8):
     real_proportions = torch.tensor(class_proportions, dtype=torch.float32).cuda()
     
@@ -346,7 +345,7 @@ def compute_kl_loss_on_bagbatch(estimated_proportions, class_proportions, epsilo
     loss = torch.sum(-real_proportions * torch.log(avg_prob), dim=-1).mean()
     
     return loss
-'''
+
 def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, data_loader,
                     optimizer, lr_schedule, wd_schedule, momentum_schedule,epoch,
                     fp16_scaler, dataset, args):  # se agrega la variable dataset
@@ -360,7 +359,7 @@ def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, data_loade
     for it, (images, labels) in enumerate(metric_logger.log_every(data_loader, 10, header)):
         
         # Calcular las proporciones de clase en el lote actual
-        real_proportions = calculate_class_proportions_in_batch(labels, dataset)
+        class_proportions = calculate_class_proportions_in_batch(labels, dataset)
         
         # update weight decay and learning rate according to their schedule
         it = len(data_loader) * epoch + it  # global training iteration
@@ -378,7 +377,10 @@ def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, data_loade
             loss1 = dino_loss(student_output, teacher_output, epoch)
             
             # Paso a través de la capa de Prototipos
-            prototypes_output = prototypes_layer(student_output)
+            # prototypes_output = prototypes_layer(student_output)
+
+            # Aplicar distributed_sinkhorn para las proporciones y calcular la pérdida de KL
+            prototypes_output = distributed_sinkhorn(student_output)
             
             # Calcular la pérdida KL
             loss2 = compute_kl_loss_on_bagbatch(prototypes_output, class_proportions, epsilon=1e-8)
@@ -547,81 +549,6 @@ def sinkhorn_knopp_proportions(self, teacher_output, epsilon, n_iterations):
 
     Q *= B  # the columns must sum to 1 so that Q is an assignment
     return Q.t()
-
-'''
-class DINOLossdFPMm(nn.Module):
-    def __init__(self, out_dim, ncrops, warmup_teacher_temp, teacher_temp,
-                 warmup_teacher_temp_epochs, nepochs, student_temp=0.1,
-                 center_momentum=0.9):
-        super().__init__()
-        self.student_temp = student_temp
-        self.center_momentum = center_momentum
-        self.ncrops = ncrops
-        self.register_buffer("center", torch.zeros(1, out_dim))
-        # we apply a warm up for the teacher temperature because
-        # a too high temperature makes the training instable at the beginning
-        self.teacher_temp_schedule = np.concatenate((
-            np.linspace(warmup_teacher_temp,
-                        teacher_temp, warmup_teacher_temp_epochs),
-            np.ones(nepochs - warmup_teacher_temp_epochs) * teacher_temp
-        ))
-        self.printed_info = False  # Flag to ensure printing only once
-
-
-    def forward(self, student_output, teacher_output, real_proportions, estimated_proportions, epoch, alpha=0.5, beta=0.5, epsilon=1e-8):
-        """
-        Cross-entropy between softmax outputs of the teacher and student networks.
-        """
-        student_out = student_output / self.student_temp
-        student_out = student_out.chunk(self.ncrops)
-
-        # teacher centering and sharpening
-        temp = self.teacher_temp_schedule[epoch]
-        teacher_out = F.softmax((teacher_output - self.center) / temp, dim=-1)
-        teacher_out = teacher_out.detach().chunk(2)
-
-        total_loss = 0
-        n_loss_terms = 0
-        for iq, q in enumerate(teacher_out):
-            for v in range(len(student_out)):
-                if v == iq:
-                    # skip cases where student and teacher operate on the same view
-                    continue
-                # Original self-distillation loss
-                loss1 = torch.sum(-q * F.log_softmax(student_out[v], dim=-1), dim=-1)
-
-        # Aplicar distributed_sinkhorn para las proporciones y calcular la pérdida de KL
-        # estimated_proportions = distributed_sinkhorn(student_output)
-        
-        # Label proportions-based loss with symmetric cross entropy
-        probabilities = nn.functional.softmax(estimated_proportions, dim=-1)
-        avg_prob = torch.mean(probabilities, dim=0)
-        avg_prob = torch.clamp(avg_prob, epsilon, 1 - epsilon)
-        ce_loss = torch.sum(-real_proportions * F.log_softmax(avg_prob), dim=-1)
-        rce_loss = torch.sum(-avg_prob, dim=-1) * torch.log(real_proportions)
-        loss2 = ce_loss + beta * rce_loss
-
-                # Combine the losses using the alpha parameter
-                loss = alpha * loss1 + (1 - alpha) * loss2
-
-                total_loss += loss.mean()
-                n_loss_terms += 1
-        total_loss /= n_loss_terms
-        self.update_center(teacher_output)
-        return total_loss
-    
-    @torch.no_grad()
-    def update_center(self, teacher_output):
-        """
-        Update center used for teacher output.
-        """
-        batch_center = torch.sum(teacher_output, dim=0, keepdim=True)
-        dist.all_reduce(batch_center)
-        batch_center = batch_center / (len(teacher_output) * dist.get_world_size())
-
-        # ema update
-        self.center = self.center * self.center_momentum + batch_center * (1 - self.center_momentum)
- '''       
 
 class DataAugmentationDINO(object):
     def __init__(self, global_crops_scale, local_crops_scale, local_crops_number):
