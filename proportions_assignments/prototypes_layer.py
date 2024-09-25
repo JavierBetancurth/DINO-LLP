938a034dcdc0dd160ab5df32763490b7dc1543d0
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2024-09-25 14:22:21 -0500

    Update prototypes_layer.py

--- proportions_assignments/prototypes_layer.py
+++ proportions_assignments/prototypes_layer.py
@@ -31,7 +31,7 @@ class Prototypes(nn.Module):
                 nn.init.constant_(m.bias, 0) es necxesario hacer esto # Convertir prototypes_output a proporciones reales y calcular la pérdida KL
             prototypes_proportions = torch.sum(prototypes_output, dim=0) / torch.sum(prototypes_output)
 '''
-
+'''
 class Prototypes(nn.Module):
     def __init__(self, output_dim, nmb_prototypes=0, init_weights=True):
         super(Prototypes, self).__init__()
@@ -57,6 +57,41 @@ class Prototypes(nn.Module):
         else:
             raise ValueError("La capa de prototipos no está definida correctamente.")
         return prototypes
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+'''
+
+class Prototypes(nn.Module):
+    def __init__(self, output_dim, nmb_prototypes=0, init_weights=True):
+        super(Prototypes, self).__init__()
+
+        self.prototypes = None
+        if isinstance(nmb_prototypes, list):
+            self.prototypes = MultiPrototypes(output_dim, nmb_prototypes)
+        elif nmb_prototypes > 0:
+            self.prototypes = nn.Linear(output_dim, nmb_prototypes, bias=False)
+        else:
+            raise ValueError("El número de prototipos debe ser mayor que 0 o una lista.")
+
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x, class_mask=None):
+        if self.prototypes is not None:
+            prototypes = self.prototypes(x)
+            
+            # Aplicar la máscara de clases si se proporciona
+            if class_mask is not None:
+                prototypes = prototypes[class_mask]
+            return prototypes
+        else:
+            raise ValueError("La capa de prototipos no está definida correctamente.")
 
     def _initialize_weights(self):
         for m in self.modules():
