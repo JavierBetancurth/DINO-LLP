e534822d2f69766edfa47fc2f1220be334e0b4f5
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2024-09-30 14:44:25 -0500

    Update main_llpdinorot.py

--- main_llpdinorot.py
+++ main_llpdinorot.py
@@ -431,7 +431,10 @@ def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, data_loade
                 
             # Aplicar distributed_sinkhorn para las proporciones y calcular la pérdida de KL
             prototypes_output = sinkhorn_knopp(prototypes, temp=args.epsilon, n_iterations=args.n_iterations)
-    
+
+            # class_proportions_global = class_proportions_global.unsqueeze(0)  # Cambiar a (1, 10)
+            class_proportions_global = class_proportions_global.repeat(640, 1)  # Cambiar a (640, 10)
+            
             # Convertir prototypes_output a proporciones reales y calcular la pérdida KL
             # loss2 = compute_kl_loss_on_bagbatch(prototypes_output, class_proportions_global, epsilon=1e-8)
             # Calcula las pérdidas
