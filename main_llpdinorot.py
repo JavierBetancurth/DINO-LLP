345d2e0639c0d7084cc201763382aa9867c81d0c
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2024-09-30 14:14:25 -0500

    Update main_llpdinorot.py

--- main_llpdinorot.py
+++ main_llpdinorot.py
@@ -433,11 +433,13 @@ def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, data_loade
             prototypes_output = sinkhorn_knopp(prototypes, temp=args.epsilon, n_iterations=args.n_iterations)
             # Impresión de las proporciones estimadas
             # print("Prototipos después de Sinkhorn:", prototypes_output)
+
+            model = SimpleClassifier(student_network=student, out_dim=65536, nmb_prototypes=10)
     
             # Convertir prototypes_output a proporciones reales y calcular la pérdida KL
             # loss2 = compute_kl_loss_on_bagbatch(prototypes_output, class_proportions_global, epsilon=1e-8)
             # Calcula las pérdidas
-            loss2 = proportion_loss_fn(student_output, class_proportions_global)
+            loss2 = proportion_loss_fn(model, class_proportions_global)
 
             # Calcula la pérdida KoLeo
             loss3 = koLeo_loss_fn(student_output)
